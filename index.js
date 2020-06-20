189173ea31c7c1a1a8e945add4dfc8ea51256612
unknown <gaurav.15bme2023@abes.ac.in>
2020-06-20 14:58:39 +0530

    set up server for backend

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const port = process.env.PORT || 5000;
+// const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+const app = express();
+
+//import models
+// require("./models/...");
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(
+//   process.env.MONGODB_URI || `mongodb://localhost:27017/...`,
+//   {
+//     useNewUrlParser: true,
+//   }
+// );
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+//import routes
+// require("./routes/...")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is up and running on Port: ${port}`);
+});
