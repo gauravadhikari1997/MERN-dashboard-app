a937f3732f34703a7bcb422afc3fef7ddb2069b9
unknown <gaurav.15bme2023@abes.ac.in>
2020-06-22 15:48:07 +0530

    modified User model to save encrypted password using bcrypt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
+const bcrypt = require("bcrypt");
 
 module.exports = (app) => {
+  //to get all users
   app.get(`/api/users`, async (req, res) => {
     let users = await User.find({});
     return res.status(200).send(users);
   });
 
+  //to get a user
   app.get(`/api/user/:id`, async (req, res) => {
     let user = await User.findById(req.params.id);
     return res.status(200).send({
@@ -15,6 +18,7 @@ module.exports = (app) => {
     });
   });
 
+  //to register a user
   app.post(`/api/user`, async (req, res) => {
     const body = req.body;
     if (!body) {
@@ -24,12 +28,36 @@ module.exports = (app) => {
       });
     }
     let user = await User.create(body);
+    user.save(function (err) {
+      if (err) throw err;
+    });
     return res.status(200).send({
       error: false,
       user,
     });
   });
 
+  //to login a user
+  app.post(`/api/login`, async (req, res) => {
+    const body = req.body;
+    if (!body) {
+      return res.status(400).send({
+        error: true,
+        errorMessage: "You must provide details",
+      });
+    }
+    User.findOne({ email: body.email }, function (err, user) {
+      if (err) throw err;
+
+      // //test a matching password
+      user.comparePassword(body.password, function (err, isMatch) {
+        if (err) throw err;
+        // console.log("Password:", isMatch); // -> Password123: true
+      });
+    });
+  });
+
+  //to modify a user
   app.put(`/api/user/:id`, async (req, res) => {
     const body = req.body;
     if (!body) {
@@ -45,6 +73,7 @@ module.exports = (app) => {
     });
   });
 
+  //to delete a user
   app.delete(`/api/user/:id`, async (req, res) => {
     let user = await User.deleteOne({ _id: req.params.id });
     return res.status(200).send({
